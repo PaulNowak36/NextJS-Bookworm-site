eea739a51b02539659e4be5479506b2c9810ea6a
PaulNowak36 <paulnowak36@orange.fr>
2026-01-27 14:00:34 +0100

    composant HomePage

--- inov-statique/app/page/[slug]/page.js
+++ inov-statique/app/page/[slug]/page.js
@@ -1,8 +1,10 @@
 import Pagination from "@components/Pagination";
+import HomePage from "@components/Home";
 import config from "@config/config.json";
 import SeoMeta from "@layouts/partials/SeoMeta";
 import { getSinglePage } from "@lib/contentParser";
 import Posts from "@partials/Posts";
+import Link from "next/link";
 const { blog_folder } = config.settings;
 
 // blog pagination
@@ -16,13 +18,29 @@ const BlogPagination = async ({ params }) => {
   const totalPages = Math.ceil(posts.length / pagination);
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
 
+  // Récupération du fichier accueil.md
+  const allPages = await getSinglePage("content");
+  const accueilPage = allPages.find((p) => p.slug === "accueil");
+
   return (
     <>
       <SeoMeta title="Blog Pagination" />
       <section className="section">
         <div className="container">
-          <Posts className="mb-16" posts={currentPosts} authors={authors} />
-          <Pagination totalPages={totalPages} currentPage={currentPage} />
+          <HomePage />
+          {/* <Posts className="mb-16" posts={currentPosts} authors={authors} /> */}
+          {/* <Pagination totalPages={totalPages} currentPage={currentPage} /> */}
+          {/* <div>
+            <h1>
+              Je suis là
+            </h1>
+
+            {/* <h3 className="mb-2">
+              <Link href={`/accueil`} className="block hover:text-primary">
+                {accueilPage.frontmatter.title}
+              </Link>
+            </h3> }
+          </div> */}
         </div>
       </section>
     </>
---
+++ inov-statique/content/accueil.md
@@ -0,0 +1,15 @@
+---
+title: "Groupe INOV"
+meta_title: "Accueil"
+image: "/images/inov_logo-bg-free.png"
+layout: "about"
+draft: false
+
+# social sites
+social:
+  facebook: "https://www.facebook.com"
+  twitter: "https://www.twitter.com"
+  instagram: "https://www.instagram.com"
+---
+
+Le Groupe **INOV** développe et opère des centres de médecine nucléaire modernes, centrés sur l’innovation, la qualité diagnostique et l’accompagnement des patients.
