6593b6feb7412715ede29a2fbe00ad37592fddb9
PaulNowak36 <paulnowak36@orange.fr>
2026-02-13 15:31:40 +0100

    affichage fondateurs

--- inov-statique/layouts/shortcodes/TeamMember.jsx
+++ inov-statique/layouts/shortcodes/TeamMember.jsx
@@ -3,15 +3,14 @@ export default function TeamMember({ name, role, children, image }) {
     <div className="flex flex-col items-center text-center mb-10 max-w-[400px] w-full">
 
       {/* Photo */}
-      {image && (
-        <div className="w-[260px] h-[260px] rounded-full overflow-hidden border border-gray-300 shadow-sm bg-white mb-4">
-          <img
+      <img
             src={image}
             alt={name}
-            className="w-full h-full object-cover"
-          />
-        </div>
-      )}
+            className="w-full h-full object-cover rounded-xl border-8 border-red-500 "
+      />
+        
+        
+      
 
       {/* Texte */}
       <div className="w-full">
