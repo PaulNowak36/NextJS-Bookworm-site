c298f7640ac26bc6e60f992d06dd30c8d1064249
PaulNowak36 <paulnowak36@orange.fr>
2026-02-17 14:51:29 +0100

    about layout change

--- inov-statique/layouts/About.js
+++ inov-statique/layouts/About.js
@@ -3,17 +3,34 @@ import { markdownify } from "@lib/utils/textConverter";
 import Image from "next/image";
 import MDXContent from "./partials/MDXContent";
 
+
+function withBasePath(path) {
+  const basePath = process.env.NEXT_PUBLIC_BASE_PATH || "";
+
+  if (!path || /^(https?:|data:|blob:)/.test(path)) return path;
+
+  if (path.startsWith("/") && basePath) {
+    if (path.startsWith(basePath + "/") || path === basePath) return path;
+    return `${basePath}${path}`;
+  }
+
+  return path;
+}
+
+
 const About = ({ data }) => {
   const { frontmatter, content } = data;
   const { title, image, social } = frontmatter;
 
+  const imageSrc = image ? withBasePath(image) : null;
+
   return (
     <section className="section">
       <div className="container text-center">
-        {image && (
+        {imageSrc && (
           <div className="img-cover mb-8">
             <Image
-              src={image}
+              src={imageSrc}
               width={920}
               height={515}
               alt={title}
