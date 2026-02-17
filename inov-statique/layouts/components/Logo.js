36aafc36f0653014efa2ee6005caaed8b32f4e9c
PaulNowak36 <paulnowak36@orange.fr>
2026-02-17 15:16:44 +0100

    logo update

--- inov-statique/layouts/components/Logo.js
+++ inov-statique/layouts/components/Logo.js
@@ -1,23 +1,33 @@
 import config from "@config/config.json";
 import Image from "next/image";
 import Link from "next/link";
+import withBasePath from "@lib/utils/basePath";
 
 const Logo = ({ src }) => {
-  // destructuring items from config object
   const { logo, logo_width, logo_height, logo_text, title } = config.site;
+
+  // 1) Choix de la source : prop src > config.logo
+  const rawSrc = src || logo;
+
+  // 2) Appliquer le basePath si besoin (dev: "", prod: "/NextJS-Bookworm-site")
+  const finalSrc = rawSrc ? withBasePath(rawSrc) : null;
+
+  const w = Number(String(logo_width).replace("px", "")) * 2;
+  const h = Number(String(logo_height).replace("px", "")) * 2;
 
   return (
     <Link href="/" className="navbar-brand block">
-      {src || logo ? (
+      {finalSrc ? (
         <Image
-          width={logo_width.replace("px", "") * 2}
-          height={logo_height.replace("px", "") * 2}
-          src={src ? src : logo}
+          src={finalSrc}
+          width={w}
+          height={h}
           alt={title}
           priority
+          unoptimized
           style={{
-            height: logo_height.replace("px", "") + "px",
-            width: logo_width.replace("px", "") + "px",
+            height: `${String(logo_height).replace("px", "")}px`,
+            width: `${String(logo_width).replace("px", "")}px`,
           }}
         />
       ) : logo_text ? (
